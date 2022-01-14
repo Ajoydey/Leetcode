e512810713bbdcfc46bd250715a7888977396200
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-01-15 00:07:44 +0530

    Time: 21 ms (20.21%), Space: 51.4 MB (18.77%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.java
+++ 477-total-hamming-distance/477-total-hamming-distance.java
@@ -1,16 +1,21 @@
+//Solution by bit-masking
+
+
 class Solution {
     public int totalHammingDistance(int[] nums) {
-        int sum=0;
-        for(int i=0; i<nums.length-1; i++)
+        int sum =0;
+        for(int i=0; i<32; i++)
         {
-            for(int j=i+1; j<nums.length; j++)
+            int mask = 1<<i;
+            int countsetbit=0;
+            for(int j=0; j<nums.length; j++)
             {
-                sum+=hammingDistance(nums[i],nums[j]);
+                if((nums[j] & mask) != 0)    //checking if ith bit of each element is 1
+                    countsetbit+=1;
             }
+            sum = sum + countsetbit*(nums.length-countsetbit);  //the second part gives the count of 0 bits
         }
         return sum;
     }
-    public int hammingDistance(int x, int y){
-        return(Integer.bitCount(x^y));
-    }
 }
+    
