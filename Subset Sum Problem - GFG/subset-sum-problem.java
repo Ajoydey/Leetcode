7ceb423e9b9ded56eb1c03bcea279abe0512fb55
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-02-22 23:07:25 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
+        //DP 01 knapsack
+        
+        boolean dp[][] = new boolean[N+1][sum+1];
+        
+        //base conditions
+        for(int j = 0; j<=sum; j++)
+        dp[0][j] = false;   //if array size is 0 not possible
+        
+        for(int i = 0; i<=N; i++)
+        dp[i][0] = true;   //if sum is 0 always possible
+        
+        //filling dp array with top down approach
+        for(int i = 1; i<=N; i++)
+        {
+            for(int j=1; j<=sum; j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    //two choice include or do not include
+                    
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j]; //donot include
+                }
+            }
+        }
+        return dp[N][sum];
+    }
+}
