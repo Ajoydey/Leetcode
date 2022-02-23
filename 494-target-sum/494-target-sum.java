a30adb236c7ee0ed0c0682c460497c7df05cb30c
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-02-23 23:02:08 +0530

    Time: 529 ms (30.03%), Space: 41.8 MB (50.70%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -3,6 +3,9 @@ class Solution {
         return targetSum(nums, nums.length, target);
         
     }
+    
+    //recursive backtracking solution
+    
     public int targetSum(int[] nums, int n, int target)
     {
         //base case if array size 0 no possible cases
@@ -11,7 +14,7 @@ class Solution {
         //productive base case
         if(n==1)
         {
-            if(nums[n-1] == target && nums[n-1] == -target)  //to handle 0 case
+            if(nums[n-1] == target && nums[n-1] == -target)  //to handle cases like [0,0,0,1] target 1
                 return 2;
             else if(nums[n-1] == Math.abs(target))
                 return 1;
