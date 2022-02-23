0d9f1116dcf932c609ef3d3e172ab77ff01498a6
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-02-23 23:00:34 +0530

    Time: 393 ms (40.36%), Space: 41.9 MB (49.28%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return targetSum(nums, nums.length, target);
+        
+    }
+    public int targetSum(int[] nums, int n, int target)
+    {
+        //base case if array size 0 no possible cases
+        if(n==0)
+            return 0;
+        //productive base case
+        if(n==1)
+        {
+            if(nums[n-1] == target && nums[n-1] == -target)  //to handle 0 case
+                return 2;
+            else if(nums[n-1] == Math.abs(target))
+                return 1;
+            else
+                return 0;
+        }
+        return targetSum(nums, n-1, target-nums[n-1])+targetSum(nums, n-1, target+nums[n-1]);
+    }
+}
