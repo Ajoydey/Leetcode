e72cee797dea63f0519e1b246401e92a9d4d0d72
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-03-05 12:06:02 +0530

    Time: 2 ms (81.14%), Space: 48.4 MB (23.53%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,11 +1,18 @@
 class Solution {
     public int[] countBits(int n) {
-        int count=0;
-        int sol[] = new int[n+1];
-        for(int j=0; j<sol.length; j++)
+        //dp solution
+        int dp[] = new int[n+1];
+        if(n==0)
         {
-            sol[j] = Integer.bitCount(j);
+            dp[0] = 0;
+            return dp;
         }
-        return sol;
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=0; i<=n; i++)
+            dp[i] = i%2 + dp[i/2];
+        
+        return dp;
     }
 }
