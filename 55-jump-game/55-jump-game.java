aa56d2f7afa8c39baaa06735d8f90ac8a25ac26e
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-03-29 12:49:37 +0530

    Time: 2 ms (87.16%), Space: 67 MB (76.93%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -5,8 +5,6 @@ class Solution {
         
         int far=0;   //farthest index that can be reached from curr index
         int n = nums.length;
-        if(n==1)
-            return true;
         for(int i=0; i<=far; i++)
         {
             far = Math.max(far, i+nums[i]);
