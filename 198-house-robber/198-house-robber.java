5ac9cd944905ba9d2ef94d7c00affcc59cdd3f65
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-06-25 15:17:55 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (60.61%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,23 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        
-        //dynamic programming
-        
-        int dp[] = new int[nums.length+2];
-        dp[0] = 0; dp[1] = 0;   //0 for -1 and 1 for 0
-        for(int i=2; i<dp.length; i++)
-        {
-            dp[i] = Math.max(nums[i-2]+dp[i-2] , dp[i-1]);
-        }
-        return dp[dp.length-1];
+        int[] dp = new int[nums.length];
+        for(int i=0; i<dp.length; i++)
+            dp[i] = -1;
+        return robSum(nums, 0, dp);
     }
-    public int rob(int[] nums, int n)
+    public int robSum(int nums[], int n, int dp[])
     {
-        
-        //recursive
-        
-        if(n==0 || n==-1)
+        if(n>=nums.length)
             return 0;
-        return Math.max(nums[n-1]+rob(nums, n-2) , rob(nums, n-1));
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = Math.max(nums[n]+robSum(nums, n+2, dp), robSum(nums, n+1, dp));
     }
+        
 }
