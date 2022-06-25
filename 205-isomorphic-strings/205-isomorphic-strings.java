d63826e0493a84666df0c9ee7f4627c1f14631d3
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-06-25 14:04:48 +0530

    Time: 11 ms (56.38%), Space: 43.2 MB (32.40%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        else
+        {
+            HashMap<Character, Character> hm = new HashMap<>();
+            boolean flag = true;
+            for(int i=0; i<s.length(); i++)
+            {
+                if(!hm.containsKey(s.charAt(i)))
+                   {
+                       if(hm.containsValue(t.charAt(i)))
+                           return false;
+                       else
+                           hm.put(s.charAt(i), t.charAt(i));
+                   }
+                 else
+                   {
+                       if(hm.get(s.charAt(i)) != t.charAt(i))
+                           return false;
+                   }
+            }
+            return true;
+        }
+    }
+}
