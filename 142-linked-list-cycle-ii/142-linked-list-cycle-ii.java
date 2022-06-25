085a821ba9e0328d02f50897408690735a02810e
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-06-25 17:10:10 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (36.28%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -10,7 +10,13 @@
  * }
  */
 public class Solution {
+    
+    
+    //More efficient approach 2 pointers Floyd's cycle detection
+    //Once cycle is detected point one pointer head, move each pointer one step at once, the node where  the 2 pointers meet is the required node
+    
     public ListNode detectCycle(ListNode head) {
+        /*
         HashSet<ListNode> cycle = new HashSet<>();
         ListNode  ptr = head;
         while(ptr != null)
@@ -21,5 +27,32 @@ public class Solution {
             ptr=ptr.next;
         }
         return null;
+        */
+        if(head == null)
+            return null;
+        if(head.next == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            return null;
+        slow = head;
+        while(slow != fast)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
