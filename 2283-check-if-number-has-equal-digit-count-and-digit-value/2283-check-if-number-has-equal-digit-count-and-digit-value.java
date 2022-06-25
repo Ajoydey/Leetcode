c5e29980ff8e484ecb351f9b5d92b3ee7a744c04
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-06-25 15:00:28 +0530

    Time: 1 ms (99.24%), Space: 40.6 MB (93.36%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean digitCount(String num) {
+        int digitval[] = new int[10];
+        int digitcount[] = new int[10];
+        for(int i=0; i<num.length(); i++)
+            digitval[i] = num.charAt(i)-'0';
+        for(int i=0; i<num.length(); i++)
+            digitcount[num.charAt(i)-'0']++;
+        for(int i=0; i<10; i++)
+            if(digitval[i]!=digitcount[i])
+                return false;
+        return true;
+    }
+}
