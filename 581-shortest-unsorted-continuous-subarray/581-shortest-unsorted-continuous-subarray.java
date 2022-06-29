957dee555db74edee15badd0046e00690d6a3e06
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-06-29 23:53:05 +0530

    Time: 3 ms (60.98%), Space: 53.9 MB (63.09%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
+        /*
         int[] nums_copy = nums.clone();
         Arrays.sort(nums_copy);
         int front=0,rear=nums.length-1;
@@ -13,5 +14,29 @@ class Solution {
                 rear--;
             return rear-front+1;
         }
+        */
+        
+        //O(n) approach
+        int left = -1, right = -1;
+        int max_so_far = Integer.MIN_VALUE;
+        int min_so_far = Integer.MAX_VALUE;
+        
+        for(int i =0; i<nums.length; i++)
+        {
+            if(nums[i]>=max_so_far)
+                max_so_far = nums[i];
+            if(nums[i]<max_so_far)
+                right = i;
+        }
+        for(int i = nums.length-1; i>=0; i--)
+        {
+            if(nums[i]<=min_so_far)
+                min_so_far = nums[i];
+            if(nums[i]>min_so_far)
+                left = i;
+        }
+        if(left == -1)
+            return 0;
+        return right-left+1;
     }
 }
