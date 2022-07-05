6dba22ad02fcc357fdff125fcf70b99fe520b246
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-07-05 22:10:05 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (71.57%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        //Binary Search works
+        
+        int l = 0;
+        int  h = nums.length-1;
+        
+        while(l<=h)
+        {
+            int mid  = l + (h-l)/2;
+            
+            if((mid==0 || nums[mid]>=nums[mid-1]) && (mid==nums.length-1||nums[mid]>=nums[mid+1]))
+                return mid;
+            else if(nums[mid+1]>nums[mid])
+            {
+                l=mid+1;
+            }
+            else
+                h=mid-1;
+        }
+        return -1;
+    }
+}
