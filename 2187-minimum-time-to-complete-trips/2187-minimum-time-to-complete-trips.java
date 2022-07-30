0325ea3e3b426052456c1b506493114b1907ab21
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-07-30 19:45:35 +0530

    Time: 252 ms (78.95%), Space: 92.1 MB (20.93%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPossible(int[] time, long mid, int totalTrips) {
+        long count = 0;
+        for(int data : time) {
+            count += (mid/data);
+            if(count >= totalTrips) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public long minimumTime(int[] time, int totalTrips) {
+        long high = Long.MAX_VALUE;
+        long low = 0;
+        while(low < high) {
+            long mid = low + (high - low)/2;
+            if(isPossible(time, mid, totalTrips)) {
+                high = mid;
+            } else {
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+}
