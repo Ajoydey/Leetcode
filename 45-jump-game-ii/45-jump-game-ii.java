4c96fa615339b713332bd236aa43211dfaa383df
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-01 17:21:17 +0530

    Time: 2 ms (82.21%), Space: 49.4 MB (49.42%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,7 +1,9 @@
 class Solution {
     public int jump(int[] nums) {
+        if(nums.length == 1)
+            return 0;
         int j = 0, cur = 0, curFar = 0,n=nums.length;
-		for (int i = 0; i < n - 1; i++) {
+		for (int i = 0; i <=curFar; i++) {
 			curFar = Math.max(curFar, i + nums[i]);
 			if (i == cur) {
 				j++;
