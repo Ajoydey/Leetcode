5d9eb7ecaabbd9d7772d32f336be10339dac0ea4
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-10 02:53:04 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (57.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,9 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int[] rows: dp)
-            Arrays.fill(rows, -1);
-        return paths(m-1, n-1, dp);
+        dp[0][0] = 1;
+        for(int i =0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 || j==0) //base case if in 0th row or 0th column only 1 way of reaching
+                    dp[i][j] = 1;
+                else
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
     public int paths(int m, int n, int[][] dp){
         if(m==0 && n==0)
