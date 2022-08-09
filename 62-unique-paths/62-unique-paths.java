08b0989507b835fd9cbed5da22af19315ae165be
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-10 02:53:30 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (92.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -17,14 +17,4 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
-    public int paths(int m, int n, int[][] dp){
-        if(m==0 && n==0)
-            return 1;
-        if(m<0 || n<0)
-            return 0;
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        return dp[m][n] = paths(m-1, n, dp) + paths(m, n-1, dp);
-    }
 }
