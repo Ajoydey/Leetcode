28c265fb0387d1587ae4bc0ba330a46a6ce1500f
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-15 18:14:40 +0530

    Time: 12 ms (6.83%), Space: 43.5 MB (7.38%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,13 +1,8 @@
 class Solution {
     public String sortSentence(String s) {
         int count = 0;
-        for(int i=0; i<s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            if(ch>='0' && ch<='9')
-                count++;
-        }
-        String[] temp = new String[count];
+        
+        String[] temp = new String[10];
         String word = "";
         for(int i=0; i<s.length(); i++)
         {
@@ -27,6 +22,8 @@ class Solution {
         String str = "";
         for(String i: temp)
         {
+            if(i==null)
+                break;
             str = str+i+" ";
         }
         return str.trim();
