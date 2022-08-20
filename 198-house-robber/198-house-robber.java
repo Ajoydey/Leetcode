2f34d17a187b24b7a18b74d040be2f6c61d01240
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-20 20:24:06 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (83.53%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,16 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return robSum(nums, 0, dp);
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0; dp[1] = nums[0];
+        for(int i=2; i<=nums.length; i++)
+        {
+            dp[i] = Math.max(nums[i-1]+dp[i-2], dp[i-1]);
+        }
+        return dp[nums.length];
     }
-    public int robSum(int nums[], int n, int dp[])
-    {
-        if(n>=nums.length)
-            return 0;
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = Math.max(nums[n]+robSum(nums, n+2, dp), robSum(nums, n+1, dp));
-    }
-        
 }
