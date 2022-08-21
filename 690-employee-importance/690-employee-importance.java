e228bf2b37f5ff94c9623f80387d5bcf14347fbc
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-21 18:57:58 +0530

    Time: 18 ms (6.15%), Space: 57.6 MB (84.01%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.java
@@ -0,0 +1,40 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        
+        boolean[] vis = new boolean[2001];
+        int total_imp = dfs(employees, id, vis);
+        return total_imp;
+    }
+        
+    public int dfs(List<Employee> employees, int id, boolean[] vis){
+        
+        if(vis[id])
+            return 0;
+        int total_so_far = 0;
+        for(Employee em: employees)
+        {
+            if(em.id == id)
+            {
+                vis[id] = true;
+                total_so_far += em.importance;
+                if(!((em.subordinates).isEmpty()))
+                {
+                    for(int sub: em.subordinates)
+                    {
+                        total_so_far += dfs(employees, sub, vis);
+                    }
+                }
+            }
+        }
+        return total_so_far;
+    }
+}
