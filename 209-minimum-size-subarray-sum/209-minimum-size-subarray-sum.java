db864ef6a802f5fbb63338fdb58d0547aef39e01
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-28 13:31:20 +0530

    Time: 2 ms (79.86%), Space: 57.8 MB (68.03%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        //O(n) approach
         int low =0, high = 0, sum=0, minSize = Integer.MAX_VALUE;
         while(high<nums.length)
         {
@@ -17,4 +18,41 @@ class Solution {
             return 0;
         return (sum>=target) ? Math.min(minSize,high-low):minSize;
     }
+    
+    /*O(nlogn) approach
+    public int binSearch(int target, int[] nums)
+    {
+        int low = 1, high = nums.length;  
+        int ans = 0;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(isPossible(mid, nums, target))
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        return ans;
+    }
+    public boolean isPossible(int k ,int[] nums, int target)
+    {
+        int sum=0;
+        for(int i=0; i<k; i++)
+        {
+            sum+=nums[i];
+            if(sum>=target)
+                return true;
+        }
+        for(int i=k; i<nums.length; i++)
+        {
+            sum = sum+nums[i]-nums[i-k];
+            if(sum>=target)
+                return true;
+        }
+        return false;
+    }
+    */
 }
