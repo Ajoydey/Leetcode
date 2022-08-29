b34d4a2d8583944d906ec264bdb4e512cd89a8d5
Ajoydey <54506278+Ajoydey@users.noreply.github.com>
2022-08-29 19:43:30 +0530

    Time: 19 ms (16.67%), Space: 50.4 MB (13.61%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,5 +1,24 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
+        int rows = mat.length, cols = mat[0].length;
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                map.putIfAbsent(i - j, new PriorityQueue<>());
+                map.get(i - j).offer(mat[i][j]);
+            }
+        }
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                mat[i][j] = map.get(i - j).poll();
+            }
+        }
+        return mat;
+    }
+}
+/*
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
         //follow hints
         int rows = mat.length;
         int cols = mat[0].length;
@@ -30,4 +49,4 @@ class Solution {
         }
         return mat;
     }
-}
+}*/
